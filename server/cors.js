cdd2538af3bfc3bf28fc8f83e89e83c91f629678
Bhimgouda D Patil <satvikp508@gmail.com>
2025-07-18 18:52:42 +0530

    updated cors

--- server/cors.js
+++ server/cors.js
@@ -1,6 +1,13 @@
 exports.corsOption = {
   origin: (origin, callback) => {
-    if (!origin || ["http://localhost:5173", "https://app.spiralstake.xyz"].includes(origin)) {
+    if (
+      !origin ||
+      [
+        "http://localhost:5173",
+        "https://app.spiralstake.xyz",
+        "https://dashboard.spiralstake.xyz",
+      ].includes(origin)
+    ) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
